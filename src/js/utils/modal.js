57a3262ede77ceffe42d9b7c10228a0362687a99
mrgreenward <mrgreenward@yandex.ru>
2017-02-28 02:00:16 +0300

    modal fix animate

--- src/js/utils/modal.js
+++ src/js/utils/modal.js
@@ -1,18 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     (function ($) {
-        $.fn.Modal = function(){
-            var modal =  $('.modal--js'),
-                backdropEl = '<div class="modal-backdrop"></div>'
-            this.on('click', function (e) {
-                e.preventDefault()
-                modal.toggleClass('modal-js--open').after(backdropEl);
-                $('.modal-backdrop').addClass('modal-backdrop--in');
+        $.fn.Modal = function () {
+            return this.each(function () {
+                var $this = $(this);
 
-                $('.modal__close,.modal-backdrop').on('click',function () {
-                modal.removeClass('modal-js--open');
-               $('.modal-backdrop').remove();
-            });
+                var $modal = $('.modal--js'),
+                    backdropEl = '<div class="modal-backdrop"></div>';
 
+                $this.on('click', function (e) {
+                    e.preventDefault();
+
+                    $modal.after(backdropEl);
+                    $('.modal-backdrop').fadeIn(200, function () {
+                        $modal.css('display', 'block').animate({opacity: 1}, 100);
+                    });
+
+                    $('.modal__close,.modal-backdrop').on('click', function () {
+                        $modal.animate(200, function () {
+                            $(this).css('display', 'none');
+                            $('.modal-backdrop').fadeOut(200, function () {
+                                $(this).remove();
+                            });
+                        });
+                    });
+
+                })
             })
         }
     })($)
