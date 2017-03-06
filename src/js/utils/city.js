8e36ba6b9ce3af13434394f8c6d8e265d2261095
artur <mrgreenward@yandex.ru>
2017-03-06 17:37:39 +0300

    фикс ajax

--- src/js/utils/city.js
+++ src/js/utils/city.js
@@ -1,32 +1,30 @@
 $(document).ready(function () {
     (function ($) {
+
         $.fn.DropDown = function () {
         var list =  ('.dropdown-list'),
-            title = ('.dropdown-menu__link'),
-            item = ('.dropdown-list__label'),
-            val= ('.dropdown-list__radio');
+            city= ('.dropdown-list__radio');
 
             this.on('click', function (e) {
                 e.preventDefault();
                 var root = $(this).parents('.dropdown-menu');
-
                 $('.arrow-down', root).toggleClass('arrow-up');
                 $(list, root).slideToggle('fast');
-
             });
 
-            $(item).on('click', function () {
-                var root = $(this).parents('.dropdown-menu'),
-                currentText = $('.dropdown-list__radio:checked', this).parent('label').text();
+            $('.dropdown-list__label', '.dropdown-menu--city').on('click', function () {
 
-                localStorage.setItem('city', $(val,this).val());
+                var root = $(this).parents('.dropdown-menu'),
+                currentText = $('.dropdown-list__radio:checked', this).parent('label').text();// set title of this value
+
+                localStorage.setItem('city', $(city,this).val());
                 localStorage.setItem('cityChecked', currentText);
 
                 $('.arrow-down', root).removeClass('arrow-up');
                 $(list, root).slideUp('fast');
                 $('.dropdown-menu__link').text(currentText);
 
-                $(val).not($(val,this)).prop('checked', false);
+                $(city).not($(city,this)).prop('checked', false);
             })
         }
         return this
@@ -39,14 +37,18 @@ $(document).ready(function () {
             $('.city-number').text(number);
             $('.city-mail').text(mail);
         }
-
         function cityLoad() {
-            currentText = localStorage.getItem('cityChecked');
+           var currentText = localStorage.getItem('cityChecked');
+            var currentCity = localStorage.getItem('city');
+
+            $('.dropdown-list__radio[value="'+currentCity+'"]').prop('checked',true); //set prop checked to element,from localstorage
+
                 if(!currentText){
                     $('.dropdown-menu__link').text('Ваш город');
                 }else{
                     $('.dropdown-menu__link').text(localStorage.getItem('cityChecked'));
                 }
+
             switch(localStorage.getItem('city')){
                 case 'piter':
                     return  setInfo('проспект Владимирский,82','8(927)-982-872','piter@mail.ru')
@@ -63,15 +65,13 @@ $(document).ready(function () {
                     return  setInfo('Каширское шоссе','8(22213)-12-872','asdsad@mail.ru');
             }
         }
+
         $(window).on('load',function () {
             cityLoad();
         })
             $('.dropdown-list__radio').on('change', function () {
                 cityLoad();
             })
-
     })(); //вывести адреса и данные в футер контактов
-    (function () {
 
-    })()
 })
--- src/js/utils/form.js
+++ src/js/utils/form.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
     (function () {
            var message = 'заполните поле';
         var errors = [];
+
         function validate(){
             errors = [];
 
-            $('.input').removeClass('input--success input--error')
+            $('.input').removeClass('input--success input--error');
+            $('.nav__link', '.form__city').removeClass('error');
 
             $('.input').each(function (i,el) {
                 var val = $(el).val();
+
                 val = $.trim(val);
 
                 if (!val){
@@ -19,13 +22,87 @@ $(document).ready(function(){
                     errors.push(false);
                 }
                 $(el).val(val)
-            })
-        }
-        $('.btn--form').on('click',function () {
-            validate();
-            if (errors.indexOf(true)+1){
-                return false
+            });// check values from inputs
+
+            var cityVal = $('.dropdown-list__radio:checked', '.form').val();
+            if (!cityVal && !localStorage.getItem('city')){ //check city checked
+                $('.nav__link', '.form__city').addClass('error');
+                errors.push(true);
+            }else{
+                $('.nav__link', '.form__city').addClass('success');
+                errors.push(false);
             }
+
+
+        }
+
+        function sendData(){
+            var serverUrl = 'http://192.168.1.153:8080/form/';
+
+            var city = $('.dropdown-list__radio:checked', '.form').val(),
+                surName = $('.input--surname', '.form').val(),
+                lastName = $('.input--fo', '.form').val(),
+                car = $('.input--car', '.form').val(),
+                number = $('.input--number', '.form').val();
+           /* fetch('serverUrl', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "city": city,
+                    "surName": surName,
+                    "lastName": lastName,
+                    "car":car,
+                    "number":number
+                }),
+            }).then(function (response) {
+                $('.btn--form').prop("disabled",true);
+                return response.json();
+
+            }).then(function (json) {
+                console.log('success: ', json)
+            }).catch(function (ex) {
+                console.log('failed: ', ex)
+            });*/
+            $.ajax({
+                contentType: 'application/json; charset=utf-8',
+                url: serverUrl,
+                timeout: 30 * 1000,
+                type: 'POST',
+                dataType: 'json',
+                data: JSON.stringify({
+                    "city": city,
+                    "surName": surName,
+                    "lastName": lastName,
+                    "car":car,
+                    "number":number
+                }),
+                cache: false,
+                beforeSend: function (jqXHR, settings) {
+                    $('.btn--form').prop('disabled',true);
+                },
+                success: function (data, textStatus, jqXHR) {
+                    console.log(data, textStatus, jqXHR);
+                },
+                fail: function (jqXHR, textStatus) {
+                    console.log(jqXHR, textStatus);
+                },
+                complete: function (jqXHR, textStatus) {
+                    $('.form').trigger('reset');
+                    $('.btn--form').prop('disabled',false);
+                }
+            });
+        }
+
+        $('.btn--form').on('click', function (e) {
+            e.preventDefault();
+            validate();
+            if (errors.indexOf(true) + 1) {
+                return false
+            };
+            sendData();
         })
     })();
 })
